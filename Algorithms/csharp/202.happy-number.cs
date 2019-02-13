fa77bd876de980b097573dd522120a9703fa504e
fengbaoheng <344092466@qq.com>
2019-02-13 08:43:48 +0800

    202.快乐数

---
+++ Algorithms/csharp/202.happy-number.cs
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=202 lang=csharp
+ *
+ * [202] 快乐数
+ *
+ * https://leetcode-cn.com/problems/happy-number/description/
+ *
+ * algorithms
+ * Easy (51.17%)
+ * Total Accepted:    10.4K
+ * Total Submissions: 20.4K
+ * Testcase Example:  '19'
+ *
+ * 编写一个算法来判断一个数是不是“快乐数”。
+ * 
+ * 一个“快乐数”定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到
+ * 1。如果可以变为 1，那么这个数就是快乐数。
+ * 
+ * 示例: 
+ * 
+ * 输入: 19
+ * 输出: true
+ * 解释: 
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 0^2 = 1
+ * 
+ * 
+ */
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        var set = new HashSet<int>();
+        while (n != 1)
+        {
+            if (set.Contains(n))
+            {
+                return false;
+            }
+            else
+            {
+                set.Add(n);
+
+                int sum = 0;
+                while (n != 0)
+                {
+                    sum += (int)Math.Pow(n % 10, 2);
+                    n /= 10;
+                }
+
+                n = sum;
+            }
+        }
+
+        return true;
+    }
+}
