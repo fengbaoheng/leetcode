bce8ea957daeace678f64c3587e36348d3dfe82f
fengbaoheng <344092466@qq.com>
2019-02-17 13:05:15 +0800

    21. 合并两个有序链表

---
+++ Algorithms/csharp/21.merge-two-sorted-lists.cs
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=csharp
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (51.70%)
+ * Total Accepted:    40.7K
+ * Total Submissions: 78.3K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+        // 若传入的有空链表 则直接返回
+        if (l1 == null) { return l2; }
+        if (l2 == null) { return l1; }
+
+        ListNode headNode;
+        if (l1.val <= l2.val)
+        {
+            headNode = l1;
+            l1 = l1.next;
+        }
+        else
+        {
+            headNode = l2;
+            l2 = l2.next;
+        }
+
+        ListNode endNode = headNode;
+
+        while (l1 != null && l2 != null)
+        {
+            if (l1.val <= l2.val)
+            {
+                endNode.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                endNode.next = l2;
+                l2 = l2.next;
+            }
+
+            endNode = endNode.next;
+        }
+
+        // 直接拼接剩下的节点
+        endNode.next = l1 ?? l2;
+
+        return headNode;
+    }
+}
