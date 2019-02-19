ec46d8f63206eafa3ad0092501641f62347cdd13
fengbaoheng <344092466@qq.com>
2019-02-19 15:08:31 +0800

    28. 匹配子串

---
+++ Algorithms/csharp/28.implement-strstr.cs
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=csharp
+ *
+ * [28] 实现strStr()
+ *
+ * https://leetcode-cn.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (37.24%)
+ * Total Accepted:    34.8K
+ * Total Submissions: 92.9K
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * 实现 strStr() 函数。
+ * 
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置
+ * (从0开始)。如果不存在，则返回  -1。
+ * 
+ * 示例 1:
+ * 
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * 
+ * 
+ * 说明:
+ * 
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * 
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ * 
+ */
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        int parentLengh = haystack.Length;
+        int childLengh = needle.Length;
+
+        if (childLengh == 0) { return 0; }
+        if (childLengh > parentLengh) { return -1; }
+
+        for (int i = 0; i <= parentLengh - childLengh; i++)
+        {
+            if (haystack[i] == needle[0])
+            {
+                int j = 1;
+                while (j < childLengh)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                    j++;
+                }
+                if (j == childLengh)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
