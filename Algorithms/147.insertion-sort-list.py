23d470d479d188a06af7827d3da8c2018f3a60b2
fengbaoheng <344092466@qq.com>
2019-04-11 13:14:34 +0800

    [147] 对链表进行插入排序

---
+++ Algorithms/147.insertion-sort-list.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode.cn id=147 lang=python3
+#
+# [147] 对链表进行插入排序
+#
+# https://leetcode-cn.com/problems/insertion-sort-list/description/
+#
+# algorithms
+# Medium (56.12%)
+# Total Accepted:    5.4K
+# Total Submissions: 9.7K
+# Testcase Example:  '[4,2,1,3]'
+#
+# 对链表进行插入排序。
+# 
+# 
+# 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+# 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+# 
+# 
+# 
+# 插入排序算法：
+# 
+# 
+# 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+# 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+# 重复直到所有输入数据插入完为止。
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入: 4->2->1->3
+# 输出: 1->2->3->4
+# 
+# 
+# 示例 2：
+# 
+# 输入: -1->5->3->4->0
+# 输出: -1->0->3->4->5
+# 
+# 
+#
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        
+        unsorted = head.next
+
+        sorted_head = head
+        sorted_tail = head
+        sorted_tail.next = None
+
+        while unsorted:
+            # 取出一个节点
+            node = unsorted
+            unsorted = unsorted.next
+            node.next = None
+
+            # 比价插入位置
+            # 直接插入尾节点
+            if node.val >= sorted_tail.val:
+                sorted_tail.next = node
+                sorted_tail = sorted_tail.next
+                sorted_tail.next = None
+            # 直接插入头节点
+            elif node.val <= sorted_head.val:
+                node.next = sorted_head
+                sorted_head = node
+            # 插在已排序完的链表中间
+            else:
+                cur = sorted_head
+                while cur:
+                    if node.val <= cur.next.val:
+                        node.next = cur.next
+                        cur.next = node
+                        break
+                    cur = cur.next 
+
+
+        return sorted_head 
+        
+
