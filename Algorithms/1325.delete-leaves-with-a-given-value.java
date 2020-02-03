145249eb977b39798ba79797b553c3e9b6c4b8b3
fengbaoheng <344092466@qq.com>
2020-02-04 07:03:10 +0800

    [1325] 删除给定值的叶子节点

---
+++ Algorithms/1325.delete-leaves-with-a-given-value.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1325 lang=java
+ *
+ * [1325] 删除给定值的叶子节点
+ */
+
+// @lc code=start
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) {
+            return null;
+        }
+
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+
+        return root;
+    }
+}
+// @lc code=end
+
