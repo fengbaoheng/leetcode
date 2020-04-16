1d1beead2dbb51b798eb829619a541644ac8d5ac
fengbaoheng <344092466@qq.com>
2020-04-16 11:59:51 +0800

    [1028] 从先序遍历还原二叉树

---
+++ Algorithms/1028.recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1028 lang=java
+ *
+ * [1028] 从先序遍历还原二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode recoverFromPreorder(String S) {
+
+    }
+}
+// @lc code=end
+
